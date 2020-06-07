cfd71ad21cc6c1352302f0f497cd37aa4dff4ce5
praza24 <prasathsrithar@hotmail.co.uk>
2020-06-07 23:42:42 +0100

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-TestingFramework_Tests/Pods-TestingFramework_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TestingFramework_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TestingFramework_Tests
+@end
---
+++ Example/Pods/Target Support Files/TestingFramework/TestingFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TestingFramework : NSObject
+@end
+@implementation PodsDummy_TestingFramework
+@end
